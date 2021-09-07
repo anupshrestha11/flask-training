44d29d54f9f0942b25b4dcd99d97c9ce692044b6
Anup Shrestha <anupshrestha1324@gmail.com>
2021-09-07 20:11:06 +0545

    delete todo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 
 todos = [{
-    "id" : 1,
+    "id": 1,
     "title": "Clean PC",
     "description": "removing dusts from the Pc an keep is shining",
     "status": "in-complete",
@@ -18,13 +18,23 @@ def get_all_todos():
 @app.route("/todos", methods=["POST"])
 def add_new_todo():
     new_todo = {
-        "id" : len(todos) + 1,
+        "id": 0 if len(todos) == 0 else todos[-1]["id"] + 1,
         "title": request.json['title'],
         "status": request.json['status'],
         "description": request.json['description']
     }
     todos.append(new_todo)
     return jsonify(new_todo)
+
+
+@app.route("/todos/<id>", methods=["DELETE"])
+def delete_todo(id):
+    for i in range(len(todos)):
+        if todos[i]["id"] == int(id):
+            print(todos[i])
+            del todos[i]
+            break
+    return jsonify(todos)
 
 
 app.run(port=8080, debug=True)
